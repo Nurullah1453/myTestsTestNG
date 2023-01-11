72baf156165e1b6943ba0bf3b014a6df95902277
Nurullah1453 <nurullah.demir1453@gmail.com>
2023-01-11 16:26:59 +0300

    11.01.2023

---
+++ src/test/java/tests/karisikTestlerim/karisikTest02.java
@@ -0,0 +1,12 @@
+package tests.karisikTestlerim;
+
+public class karisikTest02 {
+
+    /*
+    Bu projedeki butun testleri calistirin
+
+    Cevap :
+
+    Bu sorunun cevabı icin tumTestleriCalistirma.xml file göz atın
+     */
+}
