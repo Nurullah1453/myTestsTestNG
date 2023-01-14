6dde47f12335d77f3af08195f73aae0e0e4cae00
Nurullah1453 <nurullah.demir1453@gmail.com>
2023-01-14 20:24:45 +0300

    testNG testlerim

---
+++ src/test/java/tests/karisikTestlerim/dataProvider.java
@@ -0,0 +1,37 @@
+package tests.karisikTestlerim;
+
+import org.openqa.selenium.Keys;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class dataProvider {
+
+
+    @DataProvider
+    public static Object[][] aranacakKelimeler() {
+
+        Object[][] kelimelerArrayi={{"elma"},{"armut"},{"portakal"}};
+
+
+        return kelimelerArrayi;
+    }
+
+    //Amazon anasayfaya gidip arama search textbox'ta
+    //Elma, armut portakal aratalım
+
+    @Test(dataProvider = "aranacakKelimeler")
+
+    public void test(String kelimelerArrayi){
+
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        AmazonPage amazonPage = new AmazonPage();
+
+        amazonPage.amazonAramaKutusu.sendKeys(kelimelerArrayi+Keys.ENTER);
+
+
+    }
+}
